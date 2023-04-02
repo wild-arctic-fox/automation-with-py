bd10e4d981670c6965ac0d76c3748e4e94ce5a3c
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-04-02 16:45:52 +0300

    add status check

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import boto3
 
-# Output all ec2s from all regions
-regions_client = boto3.client('lightsail')
-regs = regions_client.get_regions()
-print(regs)
+ec2_client = boto3.client('ec2', region_name='eu-central-1')
+ec2_resource = boto3.resource('ec2', region_name='eu-central-1')
 
-for reg in regs["regions"]:
-    client = boto3.client('ec2', region_name=reg["name"])
-    ec2s = client.describe_instances()
-    print(reg)
-    print(ec2s)
-    print("\n")
+reservations = ec2_client.describe_instances()
+
+for reservation in reservations["Reservations"]:
+    instances = reservation['Instances']
+    for instance in instances:
+        print(instance['InstanceId'], instance['State'])
+
+statuses = ec2_client.describe_instance_status()
+
+for status in statuses["InstanceStatuses"]:
+    print(status['InstanceId'], status['InstanceStatus'], status['SystemStatus'])
