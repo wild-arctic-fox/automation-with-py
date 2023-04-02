088640eac5e1377b03c3bf511ae58b353fac31e5
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-04-02 16:58:37 +0300

    add status check scheduler

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import boto3
+import schedule
+import time
 
 ec2_client = boto3.client('ec2', region_name='eu-central-1')
 ec2_resource = boto3.resource('ec2', region_name='eu-central-1')
 
-reservations = ec2_client.describe_instances()
 
-for reservation in reservations["Reservations"]:
-    instances = reservation['Instances']
-    for instance in instances:
-        print(instance['InstanceId'], instance['State'])
+def check_instance_status():
+    reservations = ec2_client.describe_instances()
+    for reservation in reservations["Reservations"]:
+        instances = reservation['Instances']
+        for instance in instances:
+            print(instance['InstanceId'], instance['State'])
 
-statuses = ec2_client.describe_instance_status()
+    statuses = ec2_client.describe_instance_status()
+    for status in statuses["InstanceStatuses"]:
+        print(status['InstanceId'], status['InstanceStatus'], status['SystemStatus'])
 
-for status in statuses["InstanceStatuses"]:
-    print(status['InstanceId'], status['InstanceStatus'], status['SystemStatus'])
+
+schedule.every(10).seconds.do(check_instance_status)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
